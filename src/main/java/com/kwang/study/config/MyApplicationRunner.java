44c8b60300b0693c7388dd45ff7c3d2db9b1f0ed
LoveIsLive <3010796475@qq.com>
2026-05-16 12:06:45 +0800

    fix bug home

--- src/main/java/com/kwang/study/config/MyApplicationRunner.java
+++ src/main/java/com/kwang/study/config/MyApplicationRunner.java
@@ -1,7 +1,6 @@
 package com.kwang.study.config;
 
 import com.kwang.study.auth.config.JWTConfig;
-import com.kwang.study.index.config.TimeLineConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -21,9 +20,6 @@ public class MyApplicationRunner implements ApplicationRunner {
 
     @Autowired
     private JWTConfig jwtConfig;
-
-    @Autowired(required = false)
-    private TimeLineConfig timeLineConfig;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
